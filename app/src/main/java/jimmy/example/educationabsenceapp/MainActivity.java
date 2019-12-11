fe581a0a10552c9f7e468c8cad1883d1a9f80453
JIBaker92 <j_i_baker@yahoo.co.uk>
2019-12-11 15:32:21 +0000

    Second commit email application works

--- app/src/main/java/jimmy/example/educationabsenceapp/MainActivity.java
+++ app/src/main/java/jimmy/example/educationabsenceapp/MainActivity.java
@@ -2,13 +2,50 @@ package jimmy.example.educationabsenceapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText mEditTextTo;
+    private EditText mEditTextsubject;
+    private EditText mEditTextMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mEditTextTo = findViewById(R.id.edit_text_to);
+        mEditTextsubject = findViewById(R.id.edit_text_subject);
+        mEditTextsubject = findViewById(R.id.edit_text_message);
+
+        Button buttonSend = findViewById(R.id.button_send);
+        buttonSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendMail();
+            }
+        });
     }
+
+    private void sendMail(){
+        String recipientList = mEditTextTo.getText().toString(); //saves email addresses into a string and can save more than one separated by commas
+        String[] recipients = recipientList.split(","); //email1, email2
+
+        String subject= mEditTextsubject.getText().toString();
+        String message= mEditTextMessage.getText().toString();
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.putExtra(Intent.EXTRA_EMAIL, recipients); /*this method takes a string array of email recipients
+        It doesn't take a single string but only a string array that consists of one or multiple strings*/
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, message);
+
+        intent.setType("message/rfc822"); //opens email clients
+        startActivity(Intent.createChooser(intent, "Choose an email client"));
+    }
+
 }
